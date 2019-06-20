73772e81756dd4a53e2dcb51aea00df8abb4958f
Cian Coders <ciancoders@mac-mini-de-cian.local>
2019-06-20 16:36:11 -0600

    Initial Commit

---
+++ PlatziFinanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Cian Coders on 6/20/19.
+//  Copyright © 2019 Cian Coders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
